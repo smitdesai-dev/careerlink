01c6626df05a351d191d7b4818ab4fae987b3125
SMIT DESAI <info.smit.desai@gmail.com>
2025-04-06 15:39:24 +0530

    Update company_register.js

--- server/schemas/company_register.js
+++ server/schemas/company_register.js
@@ -1,21 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const companySchema = new mongoose.Schema({
-//   companyname: { type: String, required: true  },
-//   email: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-//   otp: { type: String },
-//   isVerified: { type: Boolean, default: false },
-//   sessionId: { type: String, default: null },
-//   loginHistory: [
-//     {
-//       timestamp: { type: Date, default: Date.now },
-//     },
-//   ],
-// });
-
-// module.exports = mongoose.model("company_register", companySchema);
-
 const mongoose = require("mongoose");
 
 const companySchema = new mongoose.Schema({
